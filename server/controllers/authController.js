315d55b050bdbc7a428ef0195daf11aebb951db8
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-01-10 23:14:19 +0200

    User Model done

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,17 +1,22 @@
+const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
+
 async function register(req, res) {
-    
+    res.sendStatus(200)
 }
 async function login(req, res) {
-
+    res.sendStatus(200)
 }
 async function logout(req, res) {
-
+    res.sendStatus(200)
 }
 async function refresh(req, res) {
-
+    res.sendStatus(200)
 }
 async function user(req, res) {
-
+    res.sendStatus(200)
 }
 
 module.exports = {register, login, logout, refresh, user}
