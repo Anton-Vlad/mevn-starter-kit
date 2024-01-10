2acc05f698f8b49712609860105793525a6ee771
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-01-10 22:27:35 +0200

    controllers basics

---
+++ server/controllers/authController.js
@@ -0,0 +1,15 @@
+async function register(req, res) {
+    
+}
+async function login(req, res) {
+
+}
+async function logout(req, res) {
+
+}
+async function refresh(req, res) {
+
+}
+async function user(req, res) {
+
+}
