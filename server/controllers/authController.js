47d775651609304f7aec0075c681b698e660488e
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-01-11 22:00:16 +0200

    Register COntroller basic 100%

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,7 +4,31 @@ const bcrypt = require("bcrypt")
 
 
 async function register(req, res) {
-    res.sendStatus(200)
+    const { username, email, first_name, last_name, password, password_confirm } = req.body
+
+    if (!username || !email || !password || !password_confirm || !first_name || !last_name) {
+        return res.status(422).json({message: 'Invalid fields'})
+    }
+
+    if (password !== password_confirm) {
+        return res.status(422).json({message: 'Passwords do not match'})
+    }
+
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        return res.status(409).json({message: 'Could not register'})
+    }
+
+    try {
+        let hashedPassword = await bcrypt.hash(password, 10)
+
+        await User.create({email, username, password: hashedPassword, first_name, last_name})
+
+        return res.sendStatus(201)
+    } catch (error) {
+        return res.status(400).json({message: "Could not register"})
+    }
 }
 async function login(req, res) {
     res.sendStatus(200)
