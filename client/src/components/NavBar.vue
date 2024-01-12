ecf9e67eab2477a33cc9d755aa81121767084bc4
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-01-13 00:00:22 +0200

    Client - vuetify added

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-app-bar app>
+      <v-toolbar >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <!-- <v-img src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg" max-height="40" /> -->
+
+
+        <v-toolbar-title>Vuetify</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn>
+            <router-link to="/login">Login</router-link>
+        </v-btn>
+        <v-btn>
+            <router-link to="/register">Register</router-link>
+        </v-btn>
+        <v-menu :location="'bottom'">
+            <template v-slot:activator="{ props }">
+                <v-btn append-icon="mdi-menu-down" v-bind="props">
+                    User
+                </v-btn>
+            </template>
+
+            <v-list>
+                <v-list-item>
+                    <v-list-item-title>
+                        <router-link to="/">Home</router-link>
+                    </v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title>
+                        <router-link to="/user">Account</router-link>
+                    </v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title>
+                        <router-link to="/logout">Logout</router-link>
+                    </v-list-item-title>
+                </v-list-item>
+            </v-list>
+            </v-menu>
+      </v-toolbar>
+    </v-app-bar>
+</template>
+
+<script setup lang="ts">
+import { mdiAccount } from '@mdi/js'
+</script>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,3 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +5,30 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
